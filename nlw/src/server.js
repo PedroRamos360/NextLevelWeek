000ea3cf28e6fa455387d69a66c10e6263051e77
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-06-11 20:37:04 -0300

    Aula 5

--- nlw/src/server.js
+++ nlw/src/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+
 const server = express();
 
-server.get("/", (req, res) => { // req = requisição (pergunta), res = resposta
-    res.send("Cheguei aqui");
+// configurar pasta pública
+server.use(express.static("public"));
+
+// utilizar template engine
+const nunjunks = require('nunjucks');
+nunjunks.configure('src/views', {
+    express: server,
+    noCache: true,
 });
+
+server.get("/", (req, res) => { // req = requisição (pergunta), res = resposta
+    return res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+});
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html");
+});
+
 
 // ligar servidor
 server.listen(3000);
