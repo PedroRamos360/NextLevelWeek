f9f3d2f2a102c1bfca24ab8e448a96e0acae035c
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-06-16 15:29:51 -0300

    Aula 5

--- nlw/src/database/db.js
+++ nlw/src/database/db.js
@@ -5,67 +5,67 @@ const db = new sqlite3.Database('./src/database/database.db');
 
 module.exports = db;
 // operações com o banco de dados
-// db.serialize(() => {
-//     // 1 Criar uma tabela
-//     db.run(`
-//         CREATE TABLE IF NOT EXISTS places (
-//             id INTEGER PRIMARY KEY AUTOINCREMENT,
-//             image TEXT,
-//             name TEXT,
-//             address TEXT,
-//             address2 TEXT,
-//             state TEXT,
-//             city TEXT,
-//             items TEXT 
-//         );
-//     `) // Último elemento NÃO ACEITA vírgula
-//     // 2 Inserir dados
-//     const query = `
-//         INSERT INTO places (
-//             image,
-//             name,
-//             address,
-//             address2,
-//             state,
-//             city,
-//             items
-//         ) VALUES (?,?,?,?,?,?,?);
-//     `;
+db.serialize(() => {
+    // // 1 Criar uma tabela
+    // db.run(`
+    //     CREATE TABLE IF NOT EXISTS places (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         image TEXT,
+    //         name TEXT,
+    //         address TEXT,
+    //         address2 TEXT,
+    //         state TEXT,
+    //         city TEXT,
+    //         items TEXT 
+    //     );
+    // `) // Último elemento NÃO ACEITA vírgula
+    // // 2 Inserir dados
+    // const query = `
+    //     INSERT INTO places (
+    //         image,
+    //         name,
+    //         address,
+    //         address2,
+    //         state,
+    //         city,
+    //         items
+    //     ) VALUES (?,?,?,?,?,?,?);
+    // `;
 
-//     const values = [
-//         "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-//         "Papersider",
-//         "Guilherme Gemballa, Jardima América",
-//         "Número 260",
-//         "Santa Catarina",
-//         "Rio do Sul",
-//         "Resíduos Eletrônicos, Lâmpadas"
-//     ];
+    // const values = [
+    //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    //     "Papersider",
+    //     "Guilherme Gemballa, Jardima América",
+    //     "Número 260",
+    //     "Santa Catarina",
+    //     "Rio do Sul",
+    //     "Resíduos Eletrônicos, Lâmpadas"
+    // ];
 
-//     function afterInsertData(err) {
-//         if (err) {
-//             return console.log(err);
-//         }
-//         console.log("Cadastrado com sucesso");
-//         console.log(this);
-//     };
+    // function afterInsertData(err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log("Cadastrado com sucesso");
+    //     console.log(this);
+    // };
 
-//     db.run(query, values, afterInsertData);
+    // db.run(query, values, afterInsertData);
 
-//     // 3 Consultar os dados da tabela
-//     db.all(`SELECT * FROM places`, function(err, rows) {
-//         if (err) {
-//             return console.log(err);
-//         }
-//         console.log("Aqui estão seus registros:");
-//         console.log(rows);
-//     });
+    // // 3 Consultar os dados da tabela
+    // db.all(`SELECT * FROM places`, function(err, rows) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log("Aqui estão seus registros:");
+    //     console.log(rows);
+    // });
 
-//     // 4 Deletar um dado da tabela
-//     // db.run(`DELETE FROM places WHERE id = ?`, [4], function(err) {
-//     //     if (err) {
-//     //         console.log(err);
-//     //     }
-//     //     console.log("Registro deletado com sucesso");
-//     // });
-// });
+    // 4 Deletar um dado da tabela
+    // db.run(`DELETE FROM places WHERE id = ?`, [7], function(err) {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     console.log("Registro deletado com sucesso");
+    // });
+});
--- nlw/src/server.js
+++ nlw/src/server.js
@@ -1,6 +1,9 @@
+// Criação do server com express
 const express = require('express');
-
 const server = express();
+
+// Acessando o banco de dados
+const db = require("./database/db");
 
 // configurar pasta pública
 server.use(express.static("public"));
@@ -17,11 +20,20 @@ server.get("/", (req, res) => { // req = requisição (pergunta), res = resposta
 });
 
 server.get("/create-point", (req, res) => {
+    // req.query: Query Strings enviados para a URL
+    console.log(req.query);
     return res.render("create-point.html");
 });
 
 server.get("/search-results", (req, res) => {
-    return res.render("search-results.html");
+    // pegar dados do db
+    db.all(`SELECT * FROM places`, function(err, rows) {
+        if (err) {
+            return console.log(err);
+        }
+        const total = rows.length;
+        return res.render("search-results.html", { places: rows, total });
+    });
 });
 
 
