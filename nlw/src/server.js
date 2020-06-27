76282e4a01288982dcfa548c98cad62330411c0c
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-06-27 10:44:44 -0300

    Finalização

--- nlw/src/server.js
+++ nlw/src/server.js
@@ -63,8 +63,13 @@ server.post("/save-point", (req, res) => {
 });
 
 server.get("/search-results", (req, res) => {
+    const search = req.query.search;
+    if (search == "") {
+        return res.render("search-results.html", {total: 0});
+    }
+
     // pegar dados do db
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if (err) {
             return console.log(err);
         }
